67df350bfe7e7d4705c3812d650a190ceb2c7f3d
Cherkesky <guy@cherkesky.com>
2021-09-11 16:15:56 -0500

    no tasks

--- src/App.js
+++ src/App.js
@@ -7,20 +7,7 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
 
-  const [tasks, setTasks] = useState([
-    {
-      "id": 1,
-      "text": "Doctors Appointment",
-      "day": "Feb 5th at 2:30pm",
-      "reminder": true
-    },
-    {
-      "id": 2,
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1:30pm",
-      "reminder": true
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   // Add Task
   const addTask = (task) => {
@@ -54,7 +41,7 @@ function App() {
       />
       
       {showAddTask && <AddTask onAdd={addTask}/>}
-      
+
       {tasks.length > 0
         ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
         : ('No Tasks')
